487c2eae5ad0e01836ee8d79afc6e73235912d10
Ken Dreyer <ktdreyer@ktdreyer.com>
2016-05-28 12:50:21 -0600

    setup.py: add "bump" command
    to quickly bump our version number

--- setup.py
+++ setup.py
@@ -8,9 +8,50 @@ from setuptools import setup, Command
 readme = os.path.join(os.path.dirname(__file__), 'README.rst')
 LONG_DESCRIPTION = open(readme).read()
 
-module_file = open('txbugzilla/__init__.py').read()
+
+def read_module_contents():
+    with open('txbugzilla/__init__.py') as txbugzilla_init:
+        return txbugzilla_init.read()
+
+module_file = read_module_contents()
 metadata = dict(re.findall("__([a-z]+)__\s*=\s*'([^']+)'", module_file))
 version = metadata['version']
+
+
+class BumpCommand(Command):
+    """ Bump the __version__ number and commit all changes. """
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        version = metadata['version'].split('.')
+        version[-1] = str(int(version[-1]) + 1)  # Bump the final part
+
+        try:
+            print('old version: %s  new version: %s' %
+                  (metadata['version'], '.'.join(version)))
+            raw_input('Press enter to confirm, or ctrl-c to exit >')
+        except KeyboardInterrupt:
+            raise SystemExit("\nNot proceeding")
+
+        old = "__version__ = '%s'" % metadata['version']
+        new = "__version__ = '%s'" % '.'.join(version)
+
+        module_file = read_module_contents()
+        with open('txbugzilla/__init__.py', 'w') as fileh:
+            fileh.write(module_file.replace(old, new))
+
+        # Commit everything with a standard commit message
+        cmd = ['git', 'commit', '-a', '-m',
+               'version %s' % '.'.join(version)]
+        print(' '.join(cmd))
+        subprocess.check_call(cmd)
 
 
 class ReleaseCommand(Command):
@@ -86,5 +127,5 @@ setup(
     tests_require=[
         'pytest-twisted',
     ],
-    cmdclass={'test': PyTest, 'release': ReleaseCommand},
+    cmdclass={'test': PyTest, 'bump': BumpCommand, 'release': ReleaseCommand},
 )
